e5b58d47747ff8ac547ba75903a158580eaf07f2
Joshua Cadavez <>
2022-10-01 14:59:16 -0400

    Install React Navigation, use NavigationContainer

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { StyleSheet, Text, View, SafeAreaView, SectionList, Button, Alert } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 
 import CHURCH from './src/data/church-teachings-on-jesus.json';
 import HERESIES from './src/data/heresies.json';
@@ -18,22 +19,24 @@ const DATA = [
 const Item = ({ title }) => {
 
   return (
-    <View style={styles.item}>    
-      <Button
-        title={title}
-        onPress={() => {
-          let msg;
-          if (HERESIES[title]) {
-            msg = `HERESY: ${HERESIES[title]["meaning"]}\nCHURCH: ${HERESIES[title]["church"]}`;
-          } else if (CHURCH[title]) {
-            msg = `TEACHING: ${CHURCH[title]["meaning"]}\nCHURCH: ${CHURCH[title]["church"]}`;
-          } else {
-            msg = title;
-          }
-          return Alert.alert(msg);
-        }}
-      />
-    </View>
+    <NavigationContainer>
+      <View style={styles.item}>    
+        <Button
+          title={title}
+          onPress={() => {
+            let msg;
+            if (HERESIES[title]) {
+              msg = `HERESY: ${HERESIES[title]["meaning"]}\nCHURCH: ${HERESIES[title]["church"]}`;
+            } else if (CHURCH[title]) {
+              msg = `TEACHING: ${CHURCH[title]["meaning"]}\nCHURCH: ${CHURCH[title]["church"]}`;
+            } else {
+              msg = title;
+            }
+            return Alert.alert(msg);
+          }}
+        />
+      </View>
+    </NavigationContainer>
   )
 }
 
